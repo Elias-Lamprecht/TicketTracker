7e7cd0623f89c1d8fac8f47c87d10bfad8174ae2
Elias Lamprecht <ella@infpro.com>
2026-04-24 11:31:52 +0200

    Create Ticket

---
+++ Models/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using Ticket_Tracker.Entity;
+
+namespace Ticket_Tracker.Models;
+
+class Ticket : NamedEntity
+{
+    #region properties
+
+    public int TicketNumber { get; set; }
+
+    #region navigationProperties
+
+    public Status Status { get; private set; }
+
+    #endregion navigationProperties
+
+    #endregion properties
+
+    #region ctor
+    public Ticket(Guid id, int ticketNumber, string title, string? description, Status status) : base(id, title, description)
+    {
+        TicketNumber = ticketNumber;
+        Status = status;
+    }
+    #endregion ctor
+}
+
