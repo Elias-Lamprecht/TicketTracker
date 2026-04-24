8f0a3e0ca0211916f4392c30814f2d7cf57ba43a
Elias Lamprecht <ella@infpro.com>
2026-04-24 11:42:36 +0200

    Create StatusService.

---
+++ Services/StatusService.cs
@@ -0,0 +1,38 @@
+﻿using Ticket_Tracker.Models;
+
+namespace Ticket_Tracker.Services;
+
+public class StatusService
+{
+    #region properties
+
+    private StatusService? _instance { get; set; }
+    private List<Status> _statuses { get; set; }
+
+    #endregion properties
+
+    #region ctor
+
+    public StatusService(Guid id, string title, string description)
+    {
+        _statuses = new();
+    }
+
+    #endregion ctor
+
+    #region methods
+
+    public Status CreateStatus(Guid id, string title, string description)
+    {
+        Status status = new(id, title, description);
+        _statuses.Add(status);
+        return status;
+    }
+
+    public void DeleteStatus(Status status)
+    {
+        _statuses.Remove(status);
+    }
+
+    #endregion methods
+}
