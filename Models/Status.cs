927d0b58c57329ef4af3f075b6ea0b02ceadd227
Elias Lamprecht <ella@infpro.com>
2026-04-24 11:31:00 +0200

    Create Status

---
+++ Models/Status.cs
@@ -0,0 +1,10 @@
+﻿using Ticket_Tracker.Entity;
+
+namespace Ticket_Tracker.Models;
+
+class Status : NamedEntity
+{
+    public Status(Guid id, string title, string description) : base(id, title, description)
+    {
+    }
+}
