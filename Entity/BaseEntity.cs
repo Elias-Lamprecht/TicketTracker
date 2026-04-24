4b1787c70d683066d4e283cc5017bc6e90d40dc4
Elias Lamprecht <ella@infpro.com>
2026-04-24 11:30:07 +0200

    Create BaseEntity

---
+++ Entity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Ticket_Tracker.Entity;
+
+abstract class BaseEntity
+{
+    #region properties
+    public Guid Id { get; set; }
+    #endregion properties
+
+    #region ctor
+    protected BaseEntity(Guid id)
+    {
+        Id = id;
+    }
+    #endregion ctor
+}
