1803b60710bb9e9574032861f93d72ec5c1d85f4
Elias Lamprecht <ella@infpro.com>
2026-04-27 14:39:32 +0200

    Create Priority Service

---
+++ Services/PriorityService.cs
@@ -0,0 +1,52 @@
+﻿using TicketTracker.Models;
+
+namespace TicketTracker.Services;
+
+public class PriorityService
+{
+    #region properties
+
+    private static PriorityService? _instance { get; set; }
+    private List<Priority> _priorities { get; set; }
+
+    #endregion properties
+
+    #region singleton
+
+    public static PriorityService Instance()
+    {
+        if (_instance == null)
+        {
+            _instance = new PriorityService();
+        }
+
+        return _instance;
+    }
+
+    #endregion singleton
+
+    #region ctor
+
+    private PriorityService()
+    {
+        _priorities = new();
+    }
+
+    #endregion ctor
+
+    #region methods
+
+    public Priority CreatePriority(Guid id, string title, string description)
+    {
+        Priority priority = new(id, title, description);
+        _priorities.Add(priority);
+        return priority;
+    }
+
+    public void DeletePriority(Priority priority)
+    {
+        _priorities.Remove(priority);
+    }
+
+    #endregion methods
+}
