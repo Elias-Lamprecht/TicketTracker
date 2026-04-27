818ab61b0ff595565bf11925f11c9ca8aa6f73dc
Elias Lamprecht <ella@infpro.com>
2026-04-27 10:03:45 +0200

    Create Priority

---
+++ Models/Priority.cs
@@ -0,0 +1,12 @@
+﻿using TicketTracker.Entity;
+
+namespace TicketTracker.Models;
+
+public class Priority : NamedEntity
+{
+    #region ctor
+    public Priority(Guid id, string title, string description) : base(id, title, description)
+    {
+    }
+    #endregion ctor
+}
