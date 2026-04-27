539c38776450a303cd7f64ff11a7c46e7661f087
Elias Lamprecht <ella@infpro.com>
2026-04-27 14:20:58 +0200

    Create CreateStatusWindow

---
+++ UI/Windows/CreateStatusWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using TicketTracker.Services;
+
+namespace TicketTracker.UI.Forms
+{
+    /// <summary>
+    /// Interaction logic for CreateStatusWindow.xaml
+    /// </summary>
+    public partial class CreateStatusWindow : Window
+    {
+        public CreateStatusWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateStatus_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TitleInput.Text))
+            {
+                MessageBox.Show("The Title can't be empty or just whitespace.", "ValidationError", MessageBoxButton.OK, MessageBoxImage.Warning);
+                TitleInput.Focus();
+                return;
+            } 
+            
+            if (TitleInput.Text.Length > 32) 
+            {
+                MessageBox.Show($"The Title can't be longer than 32 Characters. (Currently {TitleInput.Text.Length} long)", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                TitleInput.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(DescriptionInput.Text))
+            {
+                MessageBox.Show("The Description can't be empty or just whitespace.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                DescriptionInput.Focus();
+                return;
+            }
+            
+            if (DescriptionInput.Text.Length > 128)
+            {
+                MessageBox.Show($"The Description can't be longer than 128 Characters. (Currently {DescriptionInput.Text.Length} long)", "ValidationError", MessageBoxButton.OK, MessageBoxImage.Warning);
+                DescriptionInput.Focus(); 
+                return;
+            }
+
+            StatusService.Instance().CreateStatus(Guid.NewGuid(), TitleInput.Text, DescriptionInput.Text);
+        }
+    }
+}
