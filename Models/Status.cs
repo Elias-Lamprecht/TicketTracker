1d2c41af7f71a66ebea0bd7d248dd8db2561258b
Elias Lamprecht <ella@infpro.com>
2026-04-27 09:26:11 +0200

    Add regions to Status.

--- Models/Status.cs
+++ Models/Status.cs
@@ -4,7 +4,9 @@ namespace TicketTracker.Models;
 
 public class Status : NamedEntity
 {
+    #region ctor
     public Status(Guid id, string title, string description) : base(id, title, description)
     {
     }
+    #endregion ctor
 }
