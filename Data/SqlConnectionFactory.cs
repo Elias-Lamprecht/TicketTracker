845c1d87531ec2a83215ef226f3a8f9959db76f0
Elias Lamprecht <ella@infpro.com>
2026-04-27 10:38:37 +0200

    Create Base SqlConnectionFactory.

---
+++ Data/SqlConnectionFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TicketTracker.Data;
+
+#region interface
+
+public interface IDbConnectionFactory
+{
+    Task<SqlConnection> CreateOpenConnectionAsync();
+}
+
+#endregion interface
+
+public class SqlConnectionFactory : IDbConnectionFactory
+{
+    #region properties
+    
+    private readonly string _connectionString;
+    
+    #endregion properties
+
+    #region ctor
+
+    public SqlConnectionFactory(string connectionString = Constants.ConnectionString) => _connectionString = connectionString;
+    
+    #endregion ctor
+
+    #region methods
+
+    public async Task<SqlConnection> CreateOpenConnectionAsync()
+    {
+        SqlConnection sqlConnection = new(_connectionString);
+        await sqlConnection.OpenAsync();
+        return sqlConnection;
+    }
+
+    #endregion methods
+}
