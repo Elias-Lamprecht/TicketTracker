45183b1911e37581299fb140e1bce479eec732a9
Elias Lamprecht <ella@infpro.com>
2026-04-27 09:22:52 +0200

    Create NoteService.

---
+++ Services/NoteService.cs
@@ -0,0 +1,53 @@
+﻿using TicketTracker.Models;
+
+namespace TicketTracker.Services;
+
+public class NoteService
+{
+    #region properties
+
+    private static NoteService? _instance { get; set; }
+    private List<Note> _notes { get; set; }
+
+    #endregion properties
+
+    #region singleton
+
+    public static NoteService Instance()
+    {
+        if (_instance == null)
+        {
+            _instance = new NoteService();
+        }
+
+        return _instance;
+    }
+
+    #endregion singleton
+
+    #region ctor
+
+    public NoteService()
+    {
+        _notes = new();
+    }
+
+    #endregion ctor
+
+    #region methods
+
+    public Note CreateNote(Guid id, string text)
+    {
+        Note note = new(id, text);
+        _notes.Add(note);
+        return note;
+    }
+
+    public void DeleteNote(Note note) 
+    { 
+        _notes.Remove(note);
+    }
+
+    #endregion methods
+}
+
