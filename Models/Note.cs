db3d4896b82b0bc67c230704219d90eb15590681
Elias Lamprecht <ella@infpro.com>
2026-04-27 09:16:07 +0200

    Create Note.

---
+++ Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using TicketTracker.Entity;
+
+namespace TicketTracker.Models;
+
+public class Note : BaseEntity
+{
+    public string Text { get; private set; }
+
+    public Note(Guid id, string text) : base(id)
+    {
+        SetText(text);
+    }
+
+    public void SetText(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            throw new ArgumentNullException();
+
+        Text = text;
+    }
+}
+
